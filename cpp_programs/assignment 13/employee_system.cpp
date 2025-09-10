e390d70a5aae4c48ccc9be4836522f566c7cd2a4
kushc6352-collab <kushc6352@gmail.com>
2025-09-10 11:45:33 +0000

    not done

--- cpp_programs/assignment 13/employee_system.cpp
+++ cpp_programs/assignment 13/employee_system.cpp
@@ -9,21 +9,67 @@
 using namespace std;
 
 class Employee{
+    protected:
+    int id;
+    string name;
+    
+
     public:
+
+    Employee(int i=0,string names="unnamed"):id(i),name(names)}{}
+
+    virtual void display(){
+        cout<<"the id of employee is "<<id;
+        cout<<"the name of employee is "<<name;
+
+    }
     
 
     
 
 };
 
-class manager{
+class manager : public Employee{
+    protected:
+    int number_of_teams_handeled;
+
+    public:
+    manager(int ids=1000,string namesE="manager1",int n=3):number_of_teams_handeled(n),Employee(id,name){}
+
+    void display(){
+        cout<<"the manager name is "<<name<<endl;
+        cout<<"the id of manager is "<<id<<endl;
+        cout<<"the number of teams handeled by the manager is "<<number_of_teams_handeled<<endl;
+    }
+
 
 };
 
-class developer{
+class developer : public Employee{
+    protected:
+    int number_of_programming_languages_known;
+
+    public:
+    developer(int m=0):number_of_programming_languages_known(m),Employee(id,name){
+
+    }
+
+    
+
 
 };
 
 int main(){
+    Employee *e1;
+    manager m(10002,"manager 1",10);
+    developer d(1005,"developer1",8);
+
+    e1=&m;
+    e1->display();
+
+
+
+
+    return 0;
     
 }
